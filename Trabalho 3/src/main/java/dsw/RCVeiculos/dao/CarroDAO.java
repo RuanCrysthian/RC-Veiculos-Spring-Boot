5bf08406361f569b3394369288ad57baefe7e464
Ruan Crysthian <ruanlima155@gmail.com>
2022-09-13 11:06:07 -0300

    Controllers feito

--- Trabalho 3/src/main/java/dsw/RCVeiculos/dao/CarroDAO.java
+++ Trabalho 3/src/main/java/dsw/RCVeiculos/dao/CarroDAO.java
@@ -2,7 +2,9 @@ package dsw.RCVeiculos.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import dsw.RCVeiculos.domain.Carro;
 
@@ -16,4 +18,10 @@ public interface CarroDAO extends CrudRepository<Carro, Long>{
 	Carro save(Carro c);
 	
 	void deleteById(Long id);
+
+	@Query("Select c FROM Carro c LEFT JOIN  Loja l  ON c.loja = l.id WHERE l.id =:id")
+	List<Carro> findByLojaId(@Param("id") long id);
+
+	@Query("Select c FROM Carro c WHERE c.modelo =:modelo")
+	List<Carro> findByModelo(@Param("modelo") String modelo);
 }
