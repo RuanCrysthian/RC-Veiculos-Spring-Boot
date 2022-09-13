1611cd2bcc7258e4ae53bf2dfe43ee65f3fad13a
Ruan Crysthian <ruanlima155@gmail.com>
2022-09-13 10:47:10 -0300

    Copiando o trabalho 2 para o diretorio do trabalho 3

---
+++ Trabalho 3/src/main/java/dsw/RCVeiculos/dao/CarroDAO.java
@@ -0,0 +1,19 @@
+package dsw.RCVeiculos.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import dsw.RCVeiculos.domain.Carro;
+
+@SuppressWarnings("unchecked")
+public interface CarroDAO extends CrudRepository<Carro, Long>{
+	
+	Carro findById(long id);
+	
+	List<Carro> findAll();
+	
+	Carro save(Carro c);
+	
+	void deleteById(Long id);
+}
